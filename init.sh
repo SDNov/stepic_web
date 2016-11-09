6bb296d758f42515f0c29b46746b567ee5d7c742
Serg D <sd_nov@mail.ru>
2016-11-09 23:37:43 +0300

    gunicorn #3

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo gunicorn -c /etc/gunicorn.d/test /home/box/web/hello:app
